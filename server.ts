36d944e8f0f039f498c9f3d7421d6bb3f1bbc96b
Andreas <info@andreas-heine.net>
2021-01-20 20:13:51 +0100

    restructure

--- server.ts
+++ server.ts
@@ -82,152 +82,150 @@ const server = new OPCUAServer({
     ],
 });
 
-function startup() {
+function construct_my_address_space(server) {
 
-    function construct_my_address_space(server) {
+    const addressSpace = server.engine.addressSpace;
+    const namespace = addressSpace.registerNamespace("http://andreas-heine.net/ua/");
 
-        const addressSpace = server.engine.addressSpace;
-        const namespace = addressSpace.registerNamespace("http://andreas-heine.net/ua/");
+    // const st_cs = addressSpace.getNamespace("http://vdma-opc-st-initiative-cs/ua");
+    // const vessleType = st_cs.findObjectType("VesselObjectType");
+    // const vesselobj = vessleType.instantiate({
+    //     browseName: "myVessel",
+    //     componentOf: ,
+    // });
 
-        // const st_cs = addressSpace.getNamespace("http://vdma-opc-st-initiative-cs/ua");
-        // const vessleType = st_cs.findObjectType("VesselObjectType");
-        // const vesselobj = vessleType.instantiate({
-        //     browseName: "myVessel",
-        //     componentOf: ,
-        // });
+    const mesNode = namespace.addObject({
+        organizedBy: addressSpace.rootFolder.objects,
+        browseName: "MES"
+    });
 
-        const mesNode = namespace.addObject({
-            organizedBy: addressSpace.rootFolder.objects,
-            browseName: "MES"
-        });
-
-        const methodfolder = namespace.addObject(
+    const methodfolder = namespace.addObject(
+        {
+            browseName: "Methods",
+            componentOf: mesNode,
+            typeDefinition: "FolderType",
+        }
+    );
+    
+    const GetCarrierDataMethod = namespace.addMethod(methodfolder, {
+        browseName: "GetCarrierData",
+        displayName: "GetCarrierData",
+        inputArguments: [
             {
-                browseName: "Methods",
-                componentOf: mesNode,
-                typeDefinition: "FolderType",
-            }
-        );
+                name: "CarrierId",
+                description: "tray or carrier id",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },                
+            {
+                name: "MachineId",
+                description: "unique machine id",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+        ],
+        outputArguments: [
+            {
+                name: "CarrierId",
+                description: "tray or carrier id",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+            {
+                name: "MachineId",
+                description: "unique machine id",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+            {
+                name: "ProzessSetpoint1",
+                description: "setpoint for pretreatment",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+            {
+                name: "ProzessSetpoint2",
+                description: "setpoint for pretreatment",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+            {
+                name: "ProzessSetpoint3",
+                description: "setpoint for pretreatment",
+                dataType: DataType.UInt32,
+                arrayDimensions: VariantArrayType.Scalar,
+            },
+        ]
+    });
+
+    GetCarrierDataMethod.outputArguments.userAccessLevel = makeAccessLevelFlag("CurrentRead");
+    GetCarrierDataMethod.inputArguments.userAccessLevel = makeAccessLevelFlag("CurrentRead");
+
+    GetCarrierDataMethod.bindMethod((inputArguments,context,callback) => {
         
-        const GetCarrierDataMethod = namespace.addMethod(methodfolder, {
-            browseName: "GetCarrierData",
-            displayName: "GetCarrierData",
-            inputArguments: [
-                {
-                    name: "CarrierId",
-                    description: "tray or carrier id",
-                    dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
-                },                
-                {
-                    name: "MachineId",
-                    description: "unique machine id",
-                    dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
-                },
-            ],
+        const carrier = inputArguments[0].value;
+        const machine =  inputArguments[1].value;
+
+        let ProzessSetpoint1;
+        let ProzessSetpoint2;
+        let ProzessSetpoint3;
+
+        /* 
+        SQL-Querry
+        */
+
+        if (carrier === 0 || machine === 0) {
+            ProzessSetpoint1 = 0;
+            ProzessSetpoint2 = 0;
+            ProzessSetpoint3 = 0;
+        } else {
+            ProzessSetpoint1 = 100;
+            ProzessSetpoint2 = 200;
+            ProzessSetpoint3 = 300;
+        };
+
+        const callMethodResult = {
+            statusCode: StatusCodes.Good,
             outputArguments: [
                 {
-                    name: "CarrierId",
-                    description: "tray or carrier id",
                     dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
+                    arrayType: VariantArrayType.Scalar,
+                    value: carrier
                 },
                 {
-                    name: "MachineId",
-                    description: "unique machine id",
                     dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
+                    arrayType: VariantArrayType.Scalar,
+                    value: machine
                 },
                 {
-                    name: "ProzessSetpoint1",
-                    description: "setpoint for pretreatment",
                     dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
+                    arrayType: VariantArrayType.Scalar,
+                    value: ProzessSetpoint1
                 },
                 {
-                    name: "ProzessSetpoint2",
-                    description: "setpoint for pretreatment",
                     dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
+                    arrayType: VariantArrayType.Scalar,
+                    value: ProzessSetpoint2
                 },
                 {
-                    name: "ProzessSetpoint3",
-                    description: "setpoint for pretreatment",
                     dataType: DataType.UInt32,
-                    arrayDimensions: VariantArrayType.Scalar,
-                },
+                    arrayType: VariantArrayType.Scalar,
+                    value: ProzessSetpoint3
+                }
             ]
-        });
+        };
+        callback(null,callMethodResult);
+    })
 
-        GetCarrierDataMethod.outputArguments.userAccessLevel = makeAccessLevelFlag("CurrentRead");
-        GetCarrierDataMethod.inputArguments.userAccessLevel = makeAccessLevelFlag("CurrentRead");
+    const SetCarrierDataMethod = namespace.addMethod(methodfolder, {
+        browseName: "SetCarrierData",
+        displayName: "SetCarrierData",
+    })
 
-        GetCarrierDataMethod.bindMethod((inputArguments,context,callback) => {
-            
-            const carrier = inputArguments[0].value;
-            const machine =  inputArguments[1].value;
+};
 
-            let ProzessSetpoint1;
-            let ProzessSetpoint2;
-            let ProzessSetpoint3;
-
-            /* 
-            SQL-Querry
-            */
-
-            if (carrier === 0 || machine === 0) {
-                ProzessSetpoint1 = 0;
-                ProzessSetpoint2 = 0;
-                ProzessSetpoint3 = 0;
-            } else {
-                ProzessSetpoint1 = 100;
-                ProzessSetpoint2 = 200;
-                ProzessSetpoint3 = 300;
-            };
-
-            const callMethodResult = {
-                statusCode: StatusCodes.Good,
-                outputArguments: [
-                    {
-                        dataType: DataType.UInt32,
-                        arrayType: VariantArrayType.Scalar,
-                        value: carrier
-                    },
-                    {
-                        dataType: DataType.UInt32,
-                        arrayType: VariantArrayType.Scalar,
-                        value: machine
-                    },
-                    {
-                        dataType: DataType.UInt32,
-                        arrayType: VariantArrayType.Scalar,
-                        value: ProzessSetpoint1
-                    },
-                    {
-                        dataType: DataType.UInt32,
-                        arrayType: VariantArrayType.Scalar,
-                        value: ProzessSetpoint2
-                    },
-                    {
-                        dataType: DataType.UInt32,
-                        arrayType: VariantArrayType.Scalar,
-                        value: ProzessSetpoint3
-                    }
-                ]
-            };
-            callback(null,callMethodResult);
-        })
-    
-        const SetCarrierDataMethod = namespace.addMethod(methodfolder, {
-            browseName: "SetCarrierData",
-            displayName: "SetCarrierData",
-        })
-    
-    };
-
+function startup() {
     construct_my_address_space(server);
-
     server.start();
     console.log("Server started!");
 }
